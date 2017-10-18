fa6367f6b3581780425a2823b08a02abb213c82f
soni17 <elb004@gmail.com>
2017-10-17 23:33:31 -0400

    completed edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+#root route goes to all pictures page
 root 'pictures#index'
+
+#all pictures page
 get 'pictures'      => 'pictures#index'
 
+#add picture
 post 'pictures'      => 'pictures#create'
 get 'pictures/new'   => 'pictures#new'
 
-
+#show picture
 get 'pictures/:id'  => 'pictures#show'
+
+#edit picture
+get 'pictures/:id/edit'  => "pictures#edit"
+patch 'pictures/:id'     => 'pictures#update'
+
+
+#delete picture
 
 
 
