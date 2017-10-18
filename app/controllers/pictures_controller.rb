b69efc013d2a75fe4a8372cea63e6c44893bd58f
soni17 <elb004@gmail.com>
2017-10-17 22:45:42 -0400

    added create action to inster picture into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,7 +13,17 @@ class PicturesController < ApplicationController
   end
 
   def create
-    render text: "Received POST request to '/pictures' with the data URL: #{params}"
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "/pictures"
+    else
+      render :new
+    end
   end
 
 end
