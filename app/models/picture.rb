5df6c2cfa91e255d02b32570d738c744f92ed392
soni17 <elb004@gmail.com>
2018-04-17 18:16:37 -0400

    add methods to picture model to query pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,15 @@
 class Picture < ApplicationRecord
 
+  def self.newest_first
+    Picture.order("created_at DESC")
+  end
 
+  def self.most_recent_five
+    Picture.newest_first.limit(5)
+  end
+
+  def self.created_before(time)
+    Picture.where("created_at < ?", time)
+  end
 
 end
